00d6b294c23c58d9ec19fd4120526116ba70b8b5
Scott Vandehey <fake-scott@spaceninja.com>
2023-01-20 20:58:57 -0800

    add linter

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,18 @@
 module.exports = function (eleventyConfig) {
   // Copy over various static files
   eleventyConfig.addPassthroughCopy(
-    "src/**/*.(gif|ico|jpg|png|svg|woff|woff2)"
+    'src/**/*.(gif|ico|jpg|png|svg|woff|woff2)'
   );
 
   // Refresh without re-building for CSS & JS changes
   eleventyConfig.setBrowserSyncConfig({
-    files: ["dist/**/*.js", "dist/**/*.css"],
+    files: ['dist/**/*.js', 'dist/**/*.css'],
   });
 
   return {
     dir: {
-      input: "src",
-      output: "dist",
+      input: 'src',
+      output: 'dist',
     },
   };
 };
